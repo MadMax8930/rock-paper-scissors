2494811b4bfa8a49f310556169de70411e22af07
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-03-05 14:05:36 +0100

    second

--- rock_paper.py
+++ rock_paper.py
@@ -1,1 +1,24 @@
-hello
+import random
+
+user_wins = 0
+ordi_wins = 0
+
+
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_choice = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_choice == "q":
+        break  #End the program
+
+    if user_choice not in options:
+        print("That's is not a valid option")
+        continue  #Reask to type the valid option
+
+    randomIndex = random.randint(0,2)  # rock: 0, paper: 1, scissors: 2
+    ordi_pick = options[randomIndex]
+    print(f"Computer picked {ordi_pick}.")
+
+
+print("Goodbye!")
+
